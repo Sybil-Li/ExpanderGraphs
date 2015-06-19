50a9e2786f8f3e214ea1948cb9490107319b3f17
Sybil_Li <li.bing.qing.2009@vjc.sg>
2015-06-19 13:57:21 -0400

    Changed inBetweenY to plot normalized eigenvalues instead of raw values; also reduced sample size to reduce running time

--- inBetweenY.m
+++ inBetweenY.m
@@ -3,10 +3,10 @@ function [ table ] = f1(Rsize, mode)
 % mode 1: sqrt
 % mode 2: log
 
-eigX = zeros(1,100);
-eigY2 = zeros(1,100);
-eigZZ = zeros(1,100);
-eigG = zeros(1,100);
+eigX = zeros(1,50);
+eigY2 = zeros(1,50);
+eigZZ = zeros(1,50);
+eigG = zeros(1,50);
 
 for j = 1:(Rsize/10)
     degree = j*10 + 1;
@@ -16,41 +16,46 @@ for j = 1:(Rsize/10)
         dY = ceil(log(degree));
     end
 
-    for i = 1:100
+    for i = 1:50
         X = randRegular(Rsize, degree);
         eigvalues = eig(X);
-        eigR(i) = eigvalues(Rsize-1);
+        eigX(i) = eigvalues(Rsize-1);
 
         Y = randRegular(degree, dY);
         eigvalues = eig(Y*Y);
-        eigR(i) = eigvalues(degree-1);
+        eigY2(i) = eigvalues(degree-1);
         
         zzprod = zigzag(X,Y);
         eigvalues = eig(zzprod);
         eigZZ(i) = eigvalues(Rsize*degree-1);
-
         
-        G = randRegular(Rsize*degree, 4);
+        G = randRegular(Rsize*degree, dY*dY);
         eigvalues = eig(G);
         eigG(i) = eigvalues(Rsize*degree-1);
   
     end
     meanX(j) = mean(eigX);
+    meanXnorm(j) = meanX(j)/degree;
     meanY2(j) = mean(eigY2);
+    meanY2norm(j) = meanY2(j)/(dY*dY);
     meanZZ(j) = mean(eigZZ);
+    meanZZnorm(j) = meanZZ(j)/(dY*dY);
     meanG(j) = mean(eigG);
+    meanGnorm(j) = meanG(j)/(dY*dY);
 end
 
-table = zeros((Rsize/10),4);
+table = zeros((Rsize/10),8);
 table(:,1) = meanX;
-table(:,2) = meanY2;
-table(:,3) = meanZZ;
-table(:,4) = meanG;
+table(:,2) = meanXnorm;
+table(:,3) = meanY2;
+table(:,4) = meanY2norm;
+table(:,5) = meanZZ;
+table(:,6) = meanZZnorm;
+table(:,7) = meanG;
+table(:,8) = meanGnorm;
 
 x = 1:(Rsize/10);
 figure
-plot(x, meanX ,'-o', x,meanY2,'-x', x,meanZZ,'-*', x,meanG, '-ro')
+plot(x, meanXnorm ,'-o', x,meanY2norm,'-x', x,meanZZnorm,'-*', x,meanGnorm, '-ro')
 legend('eigX','eigY^2','eigZZ', 'eigG');
-
-
 end
