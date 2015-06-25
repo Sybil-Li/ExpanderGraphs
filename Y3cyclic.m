1cb6e898aea724ea9cf822d24b5d18fce4253b1a
Sybil_Li <li.bing.qing.2009@vjc.sg>
2015-06-25 13:15:53 -0400

    Added code to ensure that X,Y,zigzag product, and G are connected. Added code to record both lamda and lamda 1.

--- Y3cyclic.m
+++ Y3cyclic.m
@@ -1,64 +1,116 @@
 function [ table ] = Y3cyclic(Rsize)
 % X is random |Y|-regular
-% Y is random 3-regular
+% Y is 3-regular cyclic
 
-eigR = zeros(1,100);
-eigY2 = zeros(1,100);
-eigZZ = zeros(1,100);
-eigG = zeros(1,100);
+samplesize = 100;
+
+eigRlamda = zeros(1,samplesize);
+eigRlamda1 = zeros(1,samplesize);
+
+eigY2lamda = zeros(1,samplesize);
+eigY2lamda1 = zeros(1,samplesize);
+
+eigZZlamda = zeros(1,samplesize);
+eigZZlamda1 = zeros(1,samplesize);
+
+eigGlamda = zeros(1,samplesize);
+eigGlamda1 = zeros(1,samplesize);
+
 
 for j = 1:(Rsize/10)
     degree = j*10;
     count = 0;
+    Rcount = 0;
+    Ycount = 0;
+    Gcount = 0;
     
-    for i = 1:100
+    for i = 1:samplesize
+        Rcount = Rcount + 1;
         R = randRegular(Rsize, degree);
         eigvalues = eig(R);
-        eigR(i) = eigvalues(Rsize-1);
+        while (eigvalues(Rsize)-eigvalues(Rsize-1) < 10^(-8))
+            Rcount = Rcount + 1;
+            R = randRegular(Rsize, degree);
+            eigvalues = eig(R);
+        end
+        eigRlamda1(i) = eigvalues(Rsize-1);
+        eigRlamda(i) = max(eigRlamda1(i), abs(eigvalues(1)));
 
+        Ycount = Ycount + 1;
         Y = cyclic3(degree);
         eigvalues = eig(Y*Y);
-        eigY2(i) = eigvalues(degree-1);
-        
+        eigY2lamda1(i) = eigvalues(degree-1);
+        eigY2lamda(i) = max(eigY2lamda1(i), abs(eigvalues(1)));
+
         zzprod = zigzag(R,Y);
         eigvalues = eig(zzprod);
-        eigZZ(i) = eigvalues(Rsize*degree-1);
-        if (eigZZ(i) < 2*sqrt(9-1))
+        eigZZlamda1(i) = eigvalues(Rsize*degree-1);
+        eigZZlamda(i) = max(eigZZlamda1(i), abs(eigvalues(1)));
+        if (eigZZlamda(i) < 2*sqrt(9-1))
            count = count+1;
         end
-        
+
+        Gcount = Gcount + 1;
         G = randRegular(Rsize*degree, 9);
         eigvalues = eig(G);
-        eigG(i) = eigvalues(Rsize*degree-1);        
+        while (eigvalues(Rsize*degree)-eigvalues(Rsize*degree-1) < 10^(-8))
+            Gcount = Gcount + 1;
+            G = randRegular(Rsize*degree, 9);
+            eigvalues = eig(G);
+        end
+        eigGlamda1(i) = eigvalues(Rsize*degree-1);
+        eigGlamda(i) = max(eigGlamda1(i), abs(eigvalues(1)));
             
     end
-    meanX(j) = mean(eigR);
-    meanY2(j) = mean(eigY2);
-    meanZZ(j) = mean(eigZZ);
-    meanG(j) = mean(eigG);
+    meanXlamda(j) = mean(eigRlamda);
+    meanXlamda1(j) = mean(eigRlamda1);
+    meanY2lamda(j) = mean(eigY2lamda);
+    meanY2lamda1(j) = mean(eigY2lamda1);
+    meanZZlamda(j) = mean(eigZZlamda);
+    meanZZlamda1(j) = mean(eigZZlamda1);
+    meanGlamda(j) = mean(eigGlamda);
+    meanGlamda1(j) = mean(eigGlamda1);
     
-    meanXnorm(j) = mean(eigR)/degree;
-    meanY2norm(j) = mean(eigY2)/9;
-    meanZZnorm(j) = mean(eigZZ)/9;
-    meanGnorm(j) = mean(eigG)/9;
-    
+    countX(i) = Rcount;
+    countY(i) = Ycount;
+    countG(i) = Gcount;
     ramanujan(j) = count/100;
 end
 
-table = zeros((Rsize/10),9);
-table(:,1) = meanX;
-table(:,2) = meanXnorm;
-table(:,3) = meanY2;
-table(:,4) = meanY2norm;
-table(:,5) = meanZZ;
-table(:,6) = meanZZnorm;
-table(:,7) = meanG;
-table(:,8) = meanGnorm;
-table(:,9) = ramanujan;
+meanXlamdanorm = meanXlamda/degree;
+meanXlamda1norm = meanXlamda1/degree;
+meanY2lamdanorm = meanY2lamda/9;
+meanY2lamda1norm = meanY2lamda1/9;
+meanZZlamdanorm = meanZZlamda/9;
+meanZZlamda1norm = meanZZlamda1/9;
+meanGlamdanorm = meanGlamda/9;
+meanGlamda1norm = meanGlamda1/9;
 
-x = 1:(Rsize/10);
-figure
-plot(x, meanXnorm ,'-o', x,meanY2norm,'-x', x,meanZZnorm,'-*', x,meanGnorm, '-ro')
-legend('eigX','eigY^2','eigZZ', 'eigG');
+table = zeros((Rsize/10),20);
+table(:,1) = countX;
+table(:,2) = meanXlamda;
+table(:,3) = meanXlamda1;
+table(:,4) = meanXlamdanorm;
+table(:,5) = meanXlamda1norm;
+table(:,6) = countY;
+table(:,7) = meanY2lamda;
+table(:,8) = meanY2lamda1;
+table(:,9) = meanY2lamdanorm;
+table(:,10) = meanY2lamda1norm;
+table(:,11) = ramanujan;
+table(:,12) = meanZZlamda;
+table(:,13) = meanZZlamda1;
+table(:,14) = meanZZlamdanorm;
+table(:,15) = meanZZlamda1norm;
+table(:,16) = countG;
+table(:,17) = meanGlamda;
+table(:,18) = meanGlamda1;
+table(:,19) = meanGlamdanorm;
+table(:,20) = meanGlamda1norm;
+
+% x = 1:(Rsize/10);
+% figure
+% plot(x, meanXnorm ,'-o', x,meanY2norm,'-x', x,meanZZnorm,'-*', x,meanGnorm, '-ro')
+% legend('eigX','eigY^2','eigZZ', 'eigG');
 
 end
---
+++ printTable.m
@@ -0,0 +1,4 @@
+function [ table ] = printTable(T)
+table = array2table(T, 'VariableNames', {'countX' 'meanXlamda' 'meanXlamda1' 'meanXlamdanorm' 'meanXlamda1norm' 'countY' 'meanY2lamda' 'meanY2lamda1' 'meanY2lamdanorm' 'meanY2lamda1norm' 'numRamanujan' 'meanZZlamda' 'meanZZlamda1' 'meanZZlamdanorm' 'meanZZlamda1norm' 'countG' 'meanGlamda' 'meanGlamda1' 'meanGlamdanorm' 'meanGlamda1norm'});
+
+end
